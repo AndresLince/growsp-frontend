415623fb74586b566c297cbdf4fb5180431069b5
Andres Lince <lince0.7@hotmail.com>
2020-12-16 20:53:35 -0500

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 //TODO cambiar a las rutas de producción
 export const environment = {
   production: true,
-  base_url:'http://localhost:3001/api'
+  base_url:'https://growsp-backend.herokuapp.com/api'
 };
