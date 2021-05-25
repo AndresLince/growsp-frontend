9b632547fc3f8d56a5d4d69bf93e33944d98639e
AndresLince <andres.lince@correounivalle.edu.co>
2021-05-24 21:18:48 -0500

    updates in the views with angular material and flex layout

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -17,6 +17,10 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatDividerModule} from '@angular/material/divider'; 
 import {MatCardModule} from '@angular/material/card'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [ModalTransactionComponent, TransactionsTableComponent, PaginatorComponent, CreateBudgetFormComponent, EditBudgetFormComponent, CreateBudgetDetailFormComponent, ModalAdjustmentAccountComponent, NetFortuneComponent, TransactionCategoryFormComponent, AccountFormComponent],
@@ -29,7 +33,11 @@ import {MatCardModule} from '@angular/material/card';
     MatIconModule,
     MatButtonModule,
     MatDividerModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
   exports:[
     ModalTransactionComponent,
--- src/app/services/modal-transaction.service.ts
+++ src/app/services/modal-transaction.service.ts
@@ -43,7 +43,7 @@ export class ModalTransactionService {
     ]
     ,
     total:[
-      ''
+      {value: '', disabled:true} 
     ]
   });
   public category_name:string='';
